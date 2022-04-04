9d3b706bf4df9fd5a7186721e0626ec4348099bc
abcbbxhdhd <ajax76@protonmail.com>
2022-04-04 17:39:06 +0300

    Setup firebase

---
+++ src/config/firebase-config.tsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAhhq6dSSBvXvRjaR-8plbIaLK99D-Hfk",
+  authDomain: "todo-react-7f424.firebaseapp.com",
+  projectId: "todo-react-7f424",
+  storageBucket: "todo-react-7f424.appspot.com",
+  messagingSenderId: "234522056941",
+  appId: "1:234522056941:web:6d1d3ffd417fd01f6014e4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
