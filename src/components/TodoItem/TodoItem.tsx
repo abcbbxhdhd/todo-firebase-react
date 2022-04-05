afd27bd968995e0fc6cdc0fd66f79e12404915b4
abcbbxhdhd <ajax76@protonmail.com>
2022-04-05 23:45:55 +0300

    Added todoItem component

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ITodo from "../../types/ITodo"
+
+interface TodoProps {
+    todo: ITodo
+}
+
+const TodoItem: React.FC<TodoProps> = ({ todo }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TodoItem
