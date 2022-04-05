7cf3531a1fe8f3e68a9d8b23e52eb95e2ddce254
abcbbxhdhd <ajax76@protonmail.com>
2022-04-05 23:47:57 +0300

    Updated TodoListView

--- src/views/TodoListView/TodoListView.tsx
+++ src/views/TodoListView/TodoListView.tsx
@@ -1,9 +1,41 @@
-import React from "react"
+import { onSnapshot, query, where } from "firebase/firestore"
+import React, { useState } from "react"
+import AddTodoItem from "../../components/AddTodoItem/AddTodoItem"
+import Layout from "../../components/Layout/Layout"
+import TodoItem from "../../components/TodoItem/TodoItem"
+import { todosRef } from "../../config/firebase-config"
+import ITodo from "../../types/ITodo"
+import IUser from "../../types/IUser"
 import "./TodoListView.css"
 
+
 const TodoListView: React.FC = () => {
+    const currentUser: IUser = JSON.parse(sessionStorage.getItem("currentUser") || '[]') 
+    const [userTodos, setUserTodos] = useState<ITodo[]>([])
+
+    const userTodosQuery = query(todosRef, where("userId", "==", currentUser.uid))
+
+    onSnapshot(userTodosQuery, (snapshot) => {
+        let todos: ITodo[] = []
+        snapshot.docs.forEach((doc) => {
+            todos.push({
+                id: doc.id, 
+                title: doc.data().title, 
+                completed: doc.data().completed
+            })
+        })
+        setUserTodos(todos)
+    })
+
+    const todosToRender = userTodos.map((todo) => {
+        return <TodoItem todo={todo} />
+    })
+
     return (
-        <h1>TodoList</h1>
+        <Layout>
+            {todosToRender}
+            <AddTodoItem />
+        </Layout>
     )
 }
 
