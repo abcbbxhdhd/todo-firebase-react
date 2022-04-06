47d2fd80b7d89272c38b5bb6a95e82d7c7c22c8b
abcbbxhdhd <ajax76@protonmail.com>
2022-04-06 16:41:24 +0300

    Updated add todo logic

--- src/components/AddTodoItem/AddTodoItem.tsx
+++ src/components/AddTodoItem/AddTodoItem.tsx
@@ -1,8 +1,39 @@
-import React from "react"
+import { addDoc } from "firebase/firestore"
+import React, { useState } from "react"
+import { todosRef } from "../../config/firebase-config"
+import IUser from "../../types/IUser"
 
 const AddTodoItem: React.FC = () => {
-    return (
-        <></>
+    const [adding, setAdding] = useState(false)
+    const [title, setTitle] = useState("")
+    const currentUser: IUser = JSON.parse(sessionStorage.getItem("currentUser") || '[]') 
+
+    const handleAddTodo = async () => {
+        setAdding(false)
+        await addDoc(todosRef, {
+            userId: currentUser.uid,
+            title,
+            completed: false
+        })
+        setTitle("")    
+    }
+    console.log(title)
+    return (    
+        <div>
+            {
+                adding &&
+
+                <>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title..." />
+                    <i onClick={handleAddTodo} className="ri-check-line"></i>
+                </> 
+            }
+            {
+                !adding &&
+
+                <i onClick={() => setAdding(true)} className="ri-add-line"></i> 
+            }
+        </div>
     )
 }
 
